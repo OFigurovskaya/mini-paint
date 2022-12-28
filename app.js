d4bd7842063bb4fb06d3952e5b677850ce73fecd
Olesya Figurovskaya <lisonkadrug@gmail.com>
2022-12-28 11:01:09 +0300

    updated functions

--- app.js
+++ app.js
@@ -6,29 +6,26 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
 
-    square.addEventListener('mouseover', () => 
-        setColor(square)
-    )
+    square.addEventListener('mouseover', setColor);
 
-    square.addEventListener('mouseleave', () => 
-    removeColor(square)
-)
+    square.addEventListener('mouseleave', removeColor);
 
     board.append(square);
 }
 
-function setColor(elem) {
+function setColor(event) {
+    const elem = event.target;
     const color = getRandomColor();
     elem.style.backgroundColor = color;
     elem.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
-function removeColor(elem) {
+function removeColor(event) {
+    const elem = event.target;
     elem.style.backgroundColor = '#1d1d1d';
     elem.style.boxShadow = `0 0 2px #000`;
 }
 
 function getRandomColor() {
-    const index = Math.floor(Math.random() * colors.length)
-    return colors[index];
+    return colors[Math.floor(Math.random() * colors.length)];
 }
